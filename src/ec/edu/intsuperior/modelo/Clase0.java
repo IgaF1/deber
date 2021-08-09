4df675a70efc4cbd85573c6e28c429417a400cfd
59399 <59399@DESKTOP-38L1KF2>
2021-08-09 15:48:59 -0500

    Dever de clases

---
+++ src/ec/edu/intsuperior/modelo/Clase0.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.modelo;
+
+/**
+ *
+ * @author 59399
+ */
+public class Clase0 {
+    
+    
+    private String atr1;
+    private String atr2;
+    private String atr3;
+    private String atr4;
+    private String atr5;
+
+    public Clase0() {
+    }
+
+    public Clase0(String atr1, String atr2, String atr3, String atr4, String atr5) {
+        this.atr1 = atr1;
+        this.atr2 = atr2;
+        this.atr3 = atr3;
+        this.atr4 = atr4;
+        this.atr5 = atr5;
+    }
+
+    public String getAtr1() {
+        return atr1;
+    }
+
+    public void setAtr1(String atr1) {
+        this.atr1 = atr1;
+    }
+
+    public String getAtr2() {
+        return atr2;
+    }
+
+    public void setAtr2(String atr2) {
+        this.atr2 = atr2;
+    }
+
+    public String getAtr3() {
+        return atr3;
+    }
+
+    public void setAtr3(String atr3) {
+        this.atr3 = atr3;
+    }
+
+    public String getAtr4() {
+        return atr4;
+    }
+
+    public void setAtr4(String atr4) {
+        this.atr4 = atr4;
+    }
+
+    public String getAtr5() {
+        return atr5;
+    }
+
+    public void setAtr5(String atr5) {
+        this.atr5 = atr5;
+    }
+
+    public static void  metodoClase1(){
+    
+            System.out.println("Este es un metodo estatico de clase 0 ");
+    
+}
+    
+    
+    
+}
